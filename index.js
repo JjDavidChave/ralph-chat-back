c9e83d2ef4733162bd19fddc1cbe650dd278e085
JjDavidChave <lianclevenord@gmail.com>
2023-11-25 15:48:54 -0500

    new serve

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const { v4: uuidv4 } = require("uuid");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://crownicevalparaiso.shop",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    const messageData = {
+      ...data,
+      id: uuidv4(),
+    };
+  
+    io.to(data.room).emit("receive_message", messageData);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("run_server");
+});
